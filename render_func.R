e236857d47ff92a23ee3619825f7ba708f9aabca
christopherjwilson <christopher.wilson@tees.ac.uk>
2025-01-10 14:18:13 +0000

    added exam answers

--- render_func.R
+++ render_func.R
@@ -27,8 +27,18 @@ render_book_slides <- function(...) {
   # copy the files to the destination directory
   file.copy(file.path(source_dir, pdf_files), dest_dir, overwrite = TRUE)
   
+  # Define the directory to search
+  source_dir <- "slides/"
+  # Specify the file extension to copy (e.g., ".csv")
+  file_extension <- ".csv"
+  # Get a list of all files with the specified extension
+  csv_files <- list.files(source_dir, pattern = file_extension)
+  # Destination directory where you want to copy the files
+  dest_dir <- "docs/slides/"
+  # copy the files to the destination directory
+  file.copy(file.path(source_dir, csv_files), dest_dir, overwrite = TRUE)
   
-  ## As above, copy all the csv files in the practicals folder to the docs/practicals folder
+  ## As above, copy all the csv and pdf files in the practicals folder to the docs/practicals folder
   
   # Define the directory to search
   source_dir <- "practicals/"
@@ -41,6 +51,16 @@ render_book_slides <- function(...) {
   # copy the files to the destination directory
   file.copy(file.path(source_dir, csv_files), dest_dir, overwrite = TRUE)
   
+  # Define the directory to search
+  source_dir <- "practicals/"
+  # Specify the file extension to copy (e.g., ".csv")
+  file_extension <- ".pdf"
+  # Get a list of all files with the specified extension
+  pdf_files <- list.files(source_dir, pattern = file_extension)
+  # Destination directory where you want to copy the files
+  dest_dir <- "docs/practicals/"
+  # copy the files to the destination directory
+  file.copy(file.path(source_dir, pdf_files), dest_dir, overwrite = TRUE)
   
 }
 
